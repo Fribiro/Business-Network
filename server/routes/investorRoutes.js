1d1be233312128936896e21a032324a0b5dcd4a8
fribiro <festusribiro@gmail.com>
2022-07-16 21:51:00 +0300

    Fetch and update single user details.

--- server/routes/investorRoutes.js
+++ server/routes/investorRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { GetInvestorById, UpdateInvestorProfile } = require('../controllers/investor');
 const { investorLogin, investorRefreshtoken, logout, investorSignup } = require('../controllers/userAuth');
 const router = express.Router();
 
@@ -7,6 +8,10 @@ router.post('/investor-login', investorLogin);
 router.post('/investor-signup', investorSignup);
 
 router.post('/refresh_token', investorRefreshtoken);
+
+router.get('/investor-dashboard/:id', GetInvestorById);
+
+router.post('/update-investor/:id', UpdateInvestorProfile);
 
 router.post('/logout', logout);
 
